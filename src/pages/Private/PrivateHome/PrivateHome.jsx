c8c097a921c51f84d4029280ce7cd95fc5256cd5
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-02-03 09:29:15 +0100

    feat: Create default page

---
+++ src/pages/Private/PrivateHome/PrivateHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function PrivateHome() {
+  return (
+    <div className="container p-5">
+      <h1 className="display-3 text-black mb-4 ">
+      private page 
+      </h1>
+    </div>
+  )
+}
