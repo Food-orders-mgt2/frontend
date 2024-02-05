879a122b929abf3b5165afbe3ef9e2757d4b873b
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-02-05 08:14:07 +0100

    fix: Update page home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,35 @@
-import React, {useContext} from 'react'
-import {UserContext} from "../context/userContext"
+import React, { useContext } from 'react'
+import { UserContext } from "../context/userContext"
+import Footer from '../components/footer'
 
 export default function Home() {
-
-  const {currentUser} = useContext(UserContext)
-
+  const { toggleModals } = useContext(UserContext)
   return (
-    <div className="container p-5">
-      <h1 className="display-3 text-light">
-        {currentUser ? "Welcome buddy" : "Hi, Sign Up or Sign In"}
-      </h1>
-    </div>
+    <>
+      <section>
+        <div className='ms-4   mt-5'><h1 className='fw-bold'>Bienvenue sur Para Dish !</h1></div>
+        <div className='ms-4 mt-5'>
+          <p>Découvrez une expérience culinaire unique en explorant
+            <br /> notre délicieux menu. Connectez-vous pour passer votre
+            <br /> commande et savourez des plats exceptionnels à portée de clic.
+          </p>
+        </div>
+        <div className='mt-5 mb-5'>
+          <button
+            onClick={() => toggleModals("signIn")}
+            className="btn btn-primary ms-4 rounded-pill ">
+            Sign In
+          </button>
+          <button
+            onClick={() => toggleModals("signUp")}
+            className="btn btn-outline-primary ms-4 rounded-pill ">
+            Sign Up
+          </button>
+        </div> 
+        <div className='footers'>
+        <Footer/>
+        </div> 
+      </section>
+    </>
   )
 }
