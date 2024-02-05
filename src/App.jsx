29367f302d6ac763d868580d13461ad9f02a8fdc
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-02-05 17:20:26 +0100

    feat: Update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import {Routes, Route} from "react-router-dom"
-import Home from "./pages/Home"
-import Navbar from './components/Navbar'
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from './components/Navbar';
 import SignUpModal from "./components/SignUpModal";
-import SignInModal from "./components/SignInModal"
-import Private from "./pages/Private/Private"
+import SigninModal from "./components/SigninModal";
+import Private from "./pages/Private/Private";
 import PrivateHome from "./pages/Private/PrivateHome/PrivateHome";
 
 function App() {
   return (
     <>
       <SignUpModal />
-      <SignInModal />
+      <SigninModal/>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home/>} />
