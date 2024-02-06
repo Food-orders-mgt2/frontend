0ca9ddd82c8a6251ea475c1c3bf5f1752ec08b1c
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-02-06 10:39:50 +0100

    fix : Update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-import {Routes, Route} from "react-router-dom";
-import Home from "./pages/Home";
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUpModal from './components/SignUpModal';
+import SigninModal from './components/SigninModal';
+import Private from './pages/Private/Private';
+import PrivateHome from './pages/Private/PrivateHome/PrivateHome';
 import Navbar from './components/Navbar';
-import SignUpModal from "./components/SignUpModal";
-import SigninModal from "./components/SigninModal";
-import Private from "./pages/Private/Private";
-import PrivateHome from "./pages/Private/PrivateHome/PrivateHome";
+import AdminHome from './pages/Private/PrivateHome/AdminHome'
 
 function App() {
+  const location = useLocation(); 
+
   return (
     <>
       <SignUpModal />
-      <SigninModal/>
-      <Navbar />
+      <SigninModal />
+      {location.pathname !== '/' &&  (
+        <>
+          <Navbar/>
+        </>
+      )}
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
         <Route path="/private" element={<Private />}>
           <Route path="/private/private-home" element={<PrivateHome />} />
+          <Route path="/private/admin-home" element={<AdminHome/>} />
         </Route>
       </Routes>
     </>
