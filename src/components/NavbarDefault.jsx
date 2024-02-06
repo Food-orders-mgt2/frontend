ff4947aaa0901aa37c4609bf838e58fde5015e41
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-02-06 20:51:42 +0100

    fix : Update url logo

--- src/components/NavbarDefault.jsx
+++ src/components/NavbarDefault.jsx
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react'
 import {Link} from "react-router-dom"
-import logo from './logo.png'
+import logo from '../assets/logo.png'
 import '../Style/NavBar.css';
 import { UserContext } from "../context/userContext" 
 
