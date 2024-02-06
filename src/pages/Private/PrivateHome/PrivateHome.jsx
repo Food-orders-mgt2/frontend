961d57f5b17ac90bc2ba89d0ff19c3f218d2868d
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-02-06 10:37:43 +0100

    fix: Update page for user

--- src/pages/Private/PrivateHome/PrivateHome.jsx
+++ src/pages/Private/PrivateHome/PrivateHome.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 export default function PrivateHome() {
   return (
-    <div className="container p-5">
-      <h1 className="display-3 text-black mb-4 ">
-      private page 
-      </h1>
-    </div>
+    <>
+      <h1>Private page home for user </h1>
+    </>
   )
 }
