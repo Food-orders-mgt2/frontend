afa9fbf908f94163ff1eb57b8962004b698712b3
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-02-07 15:50:15 +0100

    feat : Create loader components for app

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Style/Loader.css';
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div class="spinner-border text-primary"></div>
+    </div>
+  );
+};
+
+export default Loader;
