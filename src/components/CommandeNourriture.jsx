087668cbd1294f4800396e8ecedbebfaf5972d3a
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-02-07 21:50:01 +0100

    fix: Debug

--- src/components/CommandeNourriture.jsx
+++ src/components/CommandeNourriture.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import '../../src/Style/CommandeNourriture.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 export default function CommandeNourriture() {
     const [nomClient, setNomClient] = useState("");
     const [prenomClient, setPrenomClient] = useState("");
     const [nomNourriture, setNomNourriture] = useState("");
     const [pieces, setPieces] = useState("");
-    const [dateLivraison, setDateLivraison] = useState("");
+    const [dateCommande, setdateCommande] = useState("");
     const [modePaiement, setModePaiement] = useState("");
 
     const handleSubmit = (e) => {
@@ -15,14 +17,13 @@ export default function CommandeNourriture() {
         console.log("Prénom client:", prenomClient);
         console.log("Nom Nourriture:", nomNourriture);
         console.log("Pièces:", pieces);
-        console.log("Date de livraison:", dateLivraison);
+        console.log("Date de livraison:", dateCommande);
         console.log("Mode de paiement:", modePaiement);
     };
 
     return (
         <div className="Commande">
             <form onSubmit={handleSubmit}>
-                <h1>Votre commande </h1>
                 <div>
                     <label htmlFor="nomClient">Nom  :</label>
                     <input
@@ -68,12 +69,12 @@ export default function CommandeNourriture() {
                 </div>
 
                 <div>
-                    <label htmlFor="dateLivraison">Date de livraison :</label>
+                    <label htmlFor="dateCommande">Date de dateCommande :</label>
                     <input
                         type="date"
-                        id="dateLivraison"
-                        value={dateLivraison}
-                        onChange={(e) => setDateLivraison(e.target.value)}
+                        id="dateCommande"
+                        value={dateCommande}
+                        onChange={(e) => setdateCommande(e.target.value)}
                         required
                     />
                 </div>
