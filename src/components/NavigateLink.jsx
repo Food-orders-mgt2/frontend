d9f7c1baf011e834cf2d7e9251e080bd1e9c449a
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-02-07 15:51:01 +0100

    fix : Update navbar to navigate page of client

--- src/components/NavigateLink.jsx
+++ src/components/NavigateLink.jsx
@@ -4,21 +4,23 @@ import { faHome, faUtensils, faClipboardList, faUser } from '@fortawesome/free-s
 import '../Style/NavigateLink.css';
 
 const Navbar = () => {
+  const pathname = window.location.pathname;
+
   return (
     <section className="custom-navbar flex-column">
-      <a href="#" className="nav-link flex-row gap-3">
+      <a href="/private/private-home" className={`nav-link flex-row gap-3 ${pathname === '/private/private-home' ? 'active' : ''}`}>
         <FontAwesomeIcon className='icons' icon={faHome} />
         <span className="nav-text">Accueil</span>
       </a>
-      <a href="#" className="nav-link flex-row gap-3">
+      <a href="/private/private-home/menu" className={`nav-link flex-row gap-3 ${pathname === '/private/private-home/menu' ? 'active' : ''}`}>
         <FontAwesomeIcon className='icons' icon={faUtensils} />
-        <span  className="nav-text">Menu</span>
+        <span className="nav-text">Menu</span>
       </a>
-      <a href="#" className="nav-link flex-row gap-3">
+      <a href="/private/private-home/commande" className={`nav-link flex-row gap-3 ${pathname === '/private/private-home/commande' ? 'active' : ''}`}>
         <FontAwesomeIcon className='icons' icon={faClipboardList} />
         <span className="nav-text">Commander</span>
       </a>
-      <a href="#"  className="nav-link flex-row gap-3">
+      <a href="/private/private-home/contact" className={`nav-link flex-row gap-3 ${pathname === '/private/private-home/contact' ? 'active' : ''}`}>
         <FontAwesomeIcon className='icons' icon={faUser} />
         <span className="nav-text">Contacts</span>
       </a>
