fea7d42f891be2fabbea108f684db610cab63058
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-02-07 06:03:53 +0100

    feat : Create responsive

--- src/components/NavigateLink.jsx
+++ src/components/NavigateLink.jsx
@@ -5,22 +5,22 @@ import '../Style/NavigateLink.css';
 
 const Navbar = () => {
   return (
-    <section className="custom-navbar flex-column ">
-      <a href="#" className="nav-link  flex-row gap-3">
-        <FontAwesomeIcon icon={faHome} />
-        Accueil
+    <section className="custom-navbar flex-column">
+      <a href="#" className="nav-link flex-row gap-3">
+        <FontAwesomeIcon className='icons' icon={faHome} />
+        <span className="nav-text">Accueil</span>
       </a>
       <a href="#" className="nav-link flex-row gap-3">
-        <FontAwesomeIcon icon={faUtensils} />
-        Menu
+        <FontAwesomeIcon className='icons' icon={faUtensils} />
+        <span  className="nav-text">Menu</span>
       </a>
       <a href="#" className="nav-link flex-row gap-3">
-        <FontAwesomeIcon icon={faClipboardList} />
-        Commander
+        <FontAwesomeIcon className='icons' icon={faClipboardList} />
+        <span className="nav-text">Commander</span>
       </a>
-      <a href="#" className="nav-link flex-row gap-3">
-        <FontAwesomeIcon icon={faUser} />
-        Contacts
+      <a href="#"  className="nav-link flex-row gap-3">
+        <FontAwesomeIcon className='icons' icon={faUser} />
+        <span className="nav-text">Contacts</span>
       </a>
     </section>
   );
