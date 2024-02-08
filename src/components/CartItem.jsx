12b47975f8380b0654c7b5e9283995e804e34020
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-02-08 13:31:04 +0100

    feat : Create component to receaive list product

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,28 @@
+// CartItem.jsx
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { useCart } from '../context/CartContext';
+
+const CartItem = ({ product }) => {
+  const { clearCart } = useCart();
+
+  const annulerProduit = () => {
+    clearCart();
+  };
+
+  return (
+    <div className="cart-item">
+      <img className='cart-item-img' src={product.image} alt={product.nom} />
+      <div className="cart-item-details">
+        <p>{product.nom}</p>
+        <p>Prix: {product.prix}$</p>
+        <button onClick={annulerProduit} className="annuler-button">
+          <FontAwesomeIcon icon={faTimes} style={{ color: "red" }} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
