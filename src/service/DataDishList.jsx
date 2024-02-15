848e86b1d5870fae5a3e653935a21c20568927b0
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-02-15 10:46:17 +0100

    fix : Fix bug

--- src/service/DataDishList.jsx
+++ src/service/DataDishList.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CardForProduct from '../components/CardProduct';
-import '../Style/CardForProduct.css';
+import '../Style/CardForProduct.css'
 
 export default class DishList extends Component {
   state = {
@@ -8,36 +8,26 @@ export default class DishList extends Component {
   };
 
   componentDidMount() {
-    let responseClone;
-
     fetch('/dish/dish')
       .then(response => {
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
-
-        responseClone = response.clone();
         return response.json();
-      })
+      }) 
       .then(data => {
         console.log(data);
         this.setState({ dishes: data });
       })
       .catch(error => {
         console.error('Error fetching data:', error.message);
-
-        if (responseClone) {
-          responseClone.text().then(bodyText => {
-            console.log('Received the following instead of valid JSON:', bodyText);
-          });
-        }
       });
   }
 
   render() {
-    return (
-      <div className='contenair' style={{ maxHeight: '400px', overflowY: 'scroll', marginRight: 0, paddingRight: 0 }}>
-        {this.state.dishes.map(dish => (
+    return (      
+      <div className='contenair' style={{ maxHeight: '400px', overflowY: 'scroll',marginRight:0,paddingRight:0}}>
+        {this.state.dishes.map(dish => ( 
           <CardForProduct key={dish.id} dish={dish} />
         ))}
       </div>
